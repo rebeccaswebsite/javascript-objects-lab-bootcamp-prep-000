2ec7b40588ba56585653755c4798504aaa2be2e3
rebecca_huseyin <rebecca_huseyin@outlook.com>
2019-01-17 10:38:23 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function updateObjectWithKeyAndValue(obj, key, value) {
+ 
+  return Object.assign({}, obj, { [key]: value });
+}
+ 
+const recipes = { eggs: 1 };
+ 
+updateObjectWithKeyAndValue(recipes, 'chocolate', '1 cup');
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  return recipes.eggs = 2;
+}
