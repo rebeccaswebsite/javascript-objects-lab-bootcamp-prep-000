d7292ed2d093151c2a73628b2082e7ae016e2e11
rebecca_huseyin <rebecca_huseyin@outlook.com>
2019-01-17 10:47:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function updateObjectWithKeyAndValue(obj, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-    object = {key: value};
+    object.key = (value); 
   return recipes;
 }
 
